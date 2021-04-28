2b5648ea7511208f7b4d9c97ab3bbdc35713d130
chienwei-lan <chienwei@xilinx.com>
2021-04-28 12:52:18 -0700

    temp

--- host_read_write/src/host_hal.cpp
+++ host_read_write/src/host_hal.cpp
@@ -51,7 +51,7 @@ uint32_t readReg(xclDeviceHandle& handle, uint32_t addr)
 
 void writeReg(xclDeviceHandle& handle, uint32_t addr, uint32_t value) 
 {
-	printf("Writing to Address 0x%x value=0x%x\n",addr,value);
+	//printf("Writing to Address 0x%x value=0x%x\n",addr,value);
     xclWrite(handle, XCL_ADDR_KERNEL_CTRL, addr, (void*)(&value), 4);
 }
 
